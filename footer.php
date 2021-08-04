81cd715cd464b348d1fe00d9e7e7a0cb7c2a6b85
Gustavo Amezcua <gusta1296@gmail.com>
2021-08-03 17:37:38 -0700

    edited footer.php

--- footer.php
+++ footer.php
@@ -40,28 +40,36 @@
 		<div class="row">
 			<div class="col col-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
                 <?php 
-                    footer_column_content($footer_info['column_1_titles_and_text']);
+                    if($footer_info['column_1_titles_and_text']){
+                        footer_column_content($footer_info['column_1_titles_and_text']);
+                    }
                     echo ($footer_info['add_social_icons_1'] == true)? get_social_icons() : '';
                     echo ($footer_info['add_shortcode_1'] == true)? '<div class="form_container_1">'.do_shortcode($footer_info['shortcode_1']).'</div>' : '';
                 ?>
 			</div>
 			<div class="col col-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
                 <?php
-                    footer_column_content($footer_info['column_2_titles_and_text']);
+                    if($footer_info['column_2_titles_and_text']){
+                        footer_column_content($footer_info['column_2_titles_and_text']);
+                    }
                     echo ($footer_info['add_social_icons_2'] == true)? get_social_icons() : '';
                     echo ($footer_info['add_shortcode_2'] == true)? '<div class="form_container_1">'.do_shortcode($footer_info['shortcode_2']).'</div>' : '';
                 ?>
 			</div>
 			<div class="col col-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
                 <?php
-                    footer_column_content($footer_info['column_3_titles_and_text']);
+                    if($footer_info['column_3_titles_and_text']){
+                        footer_column_content($footer_info['column_3_titles_and_text']);
+                    }
                     echo ($footer_info['add_social_icons_3'] == true)? get_social_icons() : '';
                     echo ($footer_info['add_shortcode_3'] == true)? '<div class="form_container_1">'.do_shortcode($footer_info['shortcode_3']).'</div>' : '';
                 ?>
 			</div>
 			<div class="col col-12 col-sm-6 col-md-6 col-lg-3 col-xl-3">
                 <?php 
-                    footer_column_content($footer_info['column_4_titles_and_text']);
+                    if($footer_info['column_4_titles_and_text']){
+                        footer_column_content($footer_info['column_4_titles_and_text']);
+                    }
                     echo ($footer_info['add_social_icons_4'] == true)? get_social_icons() : '';
                     echo ($footer_info['add_shortcode_4'] == true)? '<div class="form_container_1">'.do_shortcode($footer_info['shortcode_4']).'</div>' : '';
                 ?>
