3c13be57a8be23b2fcc676f4a338535edbc9d74e
Gustavo Amezcua <gusta1296@gmail.com>
2021-08-05 16:41:28 -0700

    added google tag manager code

--- blocks/banner_1.php
+++ blocks/banner_1.php
@@ -1,7 +1,7 @@
-<div class="<?php echo $field['acf_fc_layout']; ?>_block" style="<?php echo !empty($field['background_image']['url'])? 'background-image:url('.$field['background_image']['url'].');': '';?>">
-    <?php
-    
-        
-    
-    ?>
+<div class="<?php echo $field['acf_fc_layout']; ?>_block">
+    <div class="box-from-side">
+        <div class="banner-image" style="<?php echo !empty($field['background_image']['url'])? 'background-image:url('.$field['background_image']['url'].');': '';?>"></div>
+        <img class="banner-gif" src="<?php echo get_stylesheet_directory_uri() . '/images/camel-07.gif'; ?>" alt="" />
+    </div>
+
 </div>
