92c6693549730aa751c76fa74c9b5ea515ac62e3
Gustavo Amezcua <GustavoA@innovision.local>
2021-09-20 18:01:38 -0700

    working on adding masonry style event gallery page

--- blocks/masonry_blog_posts_list.php
+++ blocks/masonry_blog_posts_list.php
@@ -1,8 +1,18 @@
 <div class="<?php echo $field['acf_fc_layout'].'_block'; ?>">
     <?php 
-        print_r($field);
-
         
+        $args = array(
+            'post_type'=> 'post',
+            'orderby'    => 'ID',
+            'post_status' => 'publish',
+            'order'    => 'DESC',
+            'posts_per_page' => -1, // this will retrive all the post that is published 
+            'category__in' => $field['category_of_blog_posts']
+        );
+        
+        $result = new WP_Query( $args );
+
+        print_r($result);
 
     ?>
 </div>
