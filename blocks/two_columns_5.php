a91d730423bbcae00cb06209f078affa87ed8b9f
Gustavo Amezcua <gusta1296@gmail.com>
2021-11-08 17:44:16 -0800

    Working on Catering page

--- blocks/two_columns_5.php
+++ blocks/two_columns_5.php
@@ -1,10 +1,18 @@
-<div class="<?php echo $field['acf_fc_layout'].'_block'; ?>">
-    <div class="row">
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
-            <?php echo !empty($field['form_shortcode'])? do_shortcode($field['form_shortcode']) : ''; ?>
+<div class="<?php echo $field['acf_fc_layout'].'_block'; ?>" 
+    <?php echo !empty($field['background_image']) ? 'style="background-image:url('.$field['background_image']['url'].');"' : '';?>
+>   
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text_side">
+                <div><?php echo $field['text']; ?></div>
+            </div>
         </div>
-        <div class="col col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
-            <div><?php echo $field['text']; ?></div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 form_side">
+                <?php echo !empty($field['form_shortcode'])? do_shortcode($field['form_shortcode']) : ''; ?>
+            </div>
         </div>
     </div>
 </div>
