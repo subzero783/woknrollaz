066eee98c86294a14b07b3b4ec0f8335634b213e
Gustavo Amezcua <gusta1296@gmail.com>
2021-11-10 15:53:51 -0800

    edited Catering page

--- blocks/two_columns_5.php
+++ blocks/two_columns_5.php
@@ -8,6 +8,30 @@
             </div>
         </div>
     </div>
+    <!-- <div class="container">
+        <div class="row">
+        <?php
+            if(!empty($field['images'])){
+
+
+                $image_guide = '
+                    <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                        <img class="catering-image" src="%s" alt="%s" />
+                    </div>
+                ';
+                $images_content = null;
+                foreach($field['images'] as $image){
+                    $images_content .= sprintf(
+                        $image_guide, 
+                        !empty($image['url']) ? $image['url'] : "",
+                        !empty($image['url']) ? $image['alt'] : ""
+                    );
+                }
+                echo $images_content;
+            }
+        ?>
+        </div>
+    </div> -->
     <div class="container">
         <div class="row">
             <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 form_side">
